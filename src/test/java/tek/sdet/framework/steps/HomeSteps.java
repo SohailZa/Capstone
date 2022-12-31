b91ee5a99a71336f72be4f2dce346222657f4bc1
zainabzahid <zainabzahid@localhost>
2022-12-30 23:13:55 -0800

    "This is 1"

--- src/test/java/tek/sdet/framework/steps/HomeSteps.java
+++ src/test/java/tek/sdet/framework/steps/HomeSteps.java
@@ -1,7 +1,11 @@
 package tek.sdet.framework.steps;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -9,68 +13,53 @@ import io.cucumber.java.en.When;
 import tek.sdet.framework.pages.POMFactory;
 import tek.sdet.framework.utilities.CommonUtility;
 
-
-public class HomeSteps extends CommonUtility{
-	// all your step definitions classes will extends 
+public class HomeSteps extends CommonUtility {
+	// all your step definitions classes will extends
 	// CommonUtility class
 	// we need to create object of POMFactory class
-	// PomFactory instance should be on top of the class 
+	// PomFactory instance should be on top of the class
 	POMFactory factory = new POMFactory();
-	
+
 	@Given("User is on retail website")
 	public void userIsOnRetailWebsite() {
-	String expectedTitle = "React App";
-	String actualTitle = getTitle();// wrote this one in CommonUtility
-	Assert.assertEquals(expectedTitle, actualTitle);
-	logger.info(actualTitle +" is equal to "+ expectedTitle);
+		String expectedTitle = "React App";
+		String actualTitle = getTitle(); // wrote this one in CommonUtility Assert.assertEquals(expectedTitle,
+											// actualTitle);
+		logger.info(actualTitle + " is equal to " + expectedTitle);
 	}
-	
+
 	@Then("User verify retail page logo is present")
 	public void userVerifyRetailPageLogoIsPresent() {
-	Assert.assertTrue(isElementDisplayed(factory.homePage().logo));
-	logger.info("logo is present");
-		
+		Assert.assertTrue(isElementDisplayed(factory.homePage().logo));
+		logger.info("logo is present");
 	}
-	
-	@When("User change the category to {string}")
-	public void userChangeTheCategoryTo(String value) {
-		selectByVisibleText(factory.homePage().allDepartments,value);
-		logger.info(value + " was selected from the drop down");
-	    
-	}
+
 	@When("User search for an item {string}")
 	public void userSearchForAnItem(String value) {
-		sendText(factory.homePage().searchInputField,value);
-		logger.info("user entered "+value);
-	  
+		sendText(factory.homePage().searchInputField, value);
+		logger.info("user entered " + value);
 	}
+
 	@When("User click on Search icon")
 	public void userClickOnSearchIcon() {
 		click(factory.homePage().searchButton);
 		logger.info("user clicked on search button");
 	}
+
 	@Then("Item should be present")
 	public void itemShouldBePresent() {
 		Assert.assertTrue(isElementDisplayed(factory.homePage().playStationItem));
 		logger.info("item is present");
-	    
 	}
 
-	@When("User click on All section")
-	public void userClickOnAllSection() {
-	click(factory.homePage().allElement);
-	logger.info("user clicked on All element");
-	}
-
-	@Then("User verifies {string} is present")
+//  @When("User click on All section")//  public void userClickOnAllSection() {//      click(factory.homePAge().allElement);//      logger.info("user clicked on All element");//  }    @Then("User verifies {string} is present")
 	public void user_verifies_is_present(String value) {
-	String expectedValue = value;
-	String actualValue = getElementText(factory.homePage().shopByDepartment);
-	Assert.assertEquals(expectedValue, actualValue);
-	logger.info(expectedValue + " is present");
+		String expectedValue = value;
+		String actualValue = getElementText(factory.homePage().shopByDepartment);
+		Assert.assertEquals(expectedValue, actualValue);
+		logger.info(expectedValue + " is present");
 	}
-	
-	
+
 	@And("User verifies cart icon is present")
 	public void userVerifiesCartIconIsPresent() {
 		slowDown();
@@ -81,8 +70,312 @@ public class HomeSteps extends CommonUtility{
 		scrollPageDownWithJS();
 		slowDown();
 	}
-	
 
+	@When("User click on All section")
+	public void UserClickOnAllSectionBtn() {
+		click(factory.homePage().allDep);
+		slowDown();
+		Assert.assertTrue(isElementDisplayed(factory.homePage().all));
+		logger.info("user click on all section");
+	}
+
+	@When("User on Electronics")
+	public void userOnElectronics() {
+		click(factory.homePage().Electronics);
+		Assert.assertTrue(isElementDisplayed(factory.homePage().TVAndVideo));
+		Assert.assertTrue(isElementDisplayed(factory.homePage().videoGames));
+		logger.info("user On Electronics");
+	}
+
+	@When("User on Computers")
+	public void userOnComputers() {
+		click(factory.homePage().Computers);
+		slowDown();
+		Assert.assertTrue(isElementDisplayed(factory.homePage().Accessiries));
+		Assert.assertTrue(isElementDisplayed(factory.homePage().Networking));
+		logger.info("user On Computers");
+	}
+
+	@When("User on Smart Home")
+	public void userOnSmartHome() {
+		click(factory.homePage().SmartHome);
+		slowDown();
+		Assert.assertTrue(isElementDisplayed(factory.homePage().SmartHomeLighting));
+		Assert.assertTrue(isElementDisplayed(factory.homePage().PlugsAndOultlets));
+		logger.info("user On Smart Home");
+	}
+
+	@When("User on Sports")
+	public void userOnSports() {
+		click(factory.homePage().sports);
+		slowDown();
+		Assert.assertTrue(isElementDisplayed(factory.homePage().AtheticClothing));
+		Assert.assertTrue(isElementDisplayed(factory.homePage().ExerciseFitness));
+		logger.info("user On Sports");
+	}
+
+	@When("User on Automotive")
+	public void userOnAutomotive() {
+		click(factory.homePage().automative);
+		slowDown();
+		Assert.assertTrue(isElementDisplayed(factory.homePage().AutomativePartsAccessoriecs));
+		Assert.assertTrue(isElementDisplayed(factory.homePage().MotorCyclePowerSports));
+		logger.info("user On Automotive");
+	}
+
+	@Then("below options are present in department")
+	public void belowOptionsArePresentInDepartment(DataTable dataTable) {
+		List<List<String>> department = dataTable.asLists();
+		switch (department.toString()) {
+		case "Electronics":
+			click(factory.homePage().Electronics);
+			String video = getText(factory.homePage().videoGames);
+			String TVAndVideo = getText(factory.homePage().TVAndVideo);
+			Assert.assertEquals(TVAndVideo, department.get(0).get(0));
+			Assert.assertEquals(video, department.get(0).get(1));
+			logger.info(video + " options are present in department" + TVAndVideo);
+			break;
+		case "Computers":
+			click(factory.homePage().Computers);
+			String Accessories = getText(factory.homePage().Accessiries);
+			String Networking = getText(factory.homePage().Networking);
+			Assert.assertEquals(Accessories, department.get(0).get(0));
+			Assert.assertEquals(Networking, department.get(0).get(1));
+			logger.info(Accessories + " options are present in department" + Networking);
+			break;
+		case "Sports":
+			click(factory.homePage().sports);
+			String AthleticClothing = getText(factory.homePage().AtheticClothing);
+			String ExerciseFitness = getText(factory.homePage().ExerciseFitness);
+			Assert.assertEquals(AthleticClothing, department.get(0).get(0));
+			Assert.assertEquals(ExerciseFitness, department.get(0).get(1));
+			logger.info(AthleticClothing + " options are present in department" + ExerciseFitness);
+			break;
+		case "Automotive":
+			click(factory.homePage().automative);
+			String AutomativePartsAccessories = getText(factory.homePage().AutomativePartsAccessoriecs);
+			String MotorCyclePowersports = getText(factory.homePage().MotorCyclePowerSports);
+			Assert.assertEquals(AutomativePartsAccessories, department.get(0).get(0));
+			Assert.assertEquals(MotorCyclePowersports, department.get(0).get(1));
+			logger.info(MotorCyclePowersports + " options are present in department" + AutomativePartsAccessories);
+			break;
+		}
+	}
+
+	@When("User change the Category to {string}")
+	public void userChangetheCategoryTo(String value) {
+		sendText(factory.homePage().all, value);
+		logger.info(value + " was selected from the drop down");
+		slowDown();
+	}
+
+	@And("User click on item")
+	public void Userclickonitem() {
+		click(factory.homePage().itemkassaoutdoor);
+		logger.info("User should click on the item");
+		slowDown();
+	}
+
+	@And("User select quantity ‘2’")
+	public void Userselectquantity() {
+		click(factory.homePage().qtyTwo);
+		logger.info("User should change quintidy to two");
+		slowDown();
+	}
+
+	@And("User click add to Cart button")
+	public void UserclickaddtoCartbutton() {
+		click(factory.homePage().addToTheCard);
+		logger.info("user Should see that the it add to the card");
+		slowDown();
+	}
+
+	@Then("the cart icon quantity should change to ‘2’")
+	public void thecartconquantityshouldchangeto() {
+		Assert.assertTrue(isElementDisplayed(factory.homePage().cart));
+		logger.info("User should it added");
+		slowDown();
+	}
+
+	@When("User change the category to {string}")
+	public void userChangeTheCategoryTo(String value) {
+		sendText(factory.homePage().all, value);
+		logger.info(value + " was selected from the drop down");
+		slowDown();
+	}
+
+	@Then("User click on Cart option")
+	public void userClickOnCartOption() {
+		waitTillPresence(factory.homePage().cart);
+		click(factory.homePage().cart);
+		logger.info("User should be able to click on cart option");
+		slowDown();
+	}
+
+	@Then("User click on Proceed to Checkout button")
+	public void userClickOnProceedToCheckoutButton() {
+		waitTillPresence(factory.homePage().proccedBtn);
+		click(factory.homePage().proccedBtn);
+		logger.info("User click on Proceed to Checkout button");
+		slowDown();
+	}
+
+	@Then("User click Add a new address link for shipping address")
+	public void userClickAddANewAddressLinkForShippingAddress() {
+		waitTillPresence(factory.homePage().addAddressBtn);
+		click(factory.homePage().addAddressBtn);
+		logger.info("User should be able to click on add a new address button");
+		slowDown();
+	}
+
+	@Then("User fill new address form with below information")
+	public void userFillNewAddressFormWithBelowInformation(DataTable dataTable) {
+		List<Map<String, String>> Addaddress = dataTable.asMaps(String.class, String.class);
+		selectByVisibleText(factory.homePage().country, Addaddress.get(0).get("country"));
+		click(factory.homePage().country);
+		sendText(factory.homePage().fullName, Addaddress.get(0).get("fullName"));
+		sendText(factory.homePage().phoneNumberInput, Addaddress.get(0).get("phoneNumber"));
+		sendText(factory.homePage().streetInput, Addaddress.get(0).get("streetAddress"));
+		sendText(factory.homePage().apartmentInput, Addaddress.get(0).get("apt"));
+		sendText(factory.homePage().cityInput, Addaddress.get(0).get("city"));
+		selectByVisibleText(factory.homePage().state, Addaddress.get(0).get("state"));
+		click(factory.homePage().state);
+		sendText(factory.homePage().zipCodeInput, Addaddress.get(0).get("zipCode"));
+		logger.info("User should see all the information they fill out");
+	}
+
+	@Then("User click Add Your Address button")
+	public void userClickAddYourAddressButton() {
+		click(factory.homePage().addressBtn);
+		logger.info("User click on Add Your Address Buttn");
+		slowDown();
+	}
+
+	@Then("User click Add a credit card or Debit Card for Payment method")
+	public void userClickAddACreditCardOrDebitCardForPaymentMethod() {
+		click(factory.homePage().addPaymentBtn);
+		logger.info("User should be able to click on Add Your payment Buttn");
+		slowDown();
+		click(factory.homePage().paymentSubmitBtn);
+	}
+
+	@Then("User click on Place Your Order")
+	public void userClickOnPlaceYourOrder() {
+		click(factory.homePage().placeOrderBtn);
+		logger.info("User click on Add Your Address Buttn");
+		slowDown();
+	}
+
+	@Then("A Message Should be displayed ‘Order Placed, Thanks’")
+	public void aMessageShouldBeDisplayedOrderPlacedThanks() {
+		Assert.assertTrue(isElementDisplayed(factory.homePage().orderPlacedSuccessfully));
+		logger.info("Order Placed Successfully Message is displayed");
+	}
+
+	@And("User select quantity ‘5’")
+	public void UserSelectQuantityFive() {
+		click(factory.homePage().qty5);
+		logger.info("User should change quintidy to five");
+		slowDown();
+	}
+
+	@Then("the cart icon quantity should change to ‘5’")
+	public void theCartIconQuantityShouldChangeTo() {
+		Assert.assertTrue(isElementDisplayed(factory.homePage().qtyFive));
+		logger.info("Quantity should change to five");
+	}
+
+	@When("User click on Orders section")
+	public void userClickOnOrdersSection() {
+		click(factory.homePage().orderLink);
+		logger.info("User clicked on Order section");
+	}
+
+	@When("User click on first order in list")
+	public void userClickOnFirstOrderInList() {
+		logger.info("User should see the first order");
+	}
+
+	@When("User click on Cancel The Order button")
+	public void userClickOnCancelOrderButton() {
+		click(factory.homePage().cancelBttn);
+		logger.info("User clicked on cancel button");
+	}
+
+	@And("User select the cancelation Reason ‘Bought wrong item’")
+	public void UserselectthecancelationReason() {
+		click(factory.homePage().reasonforreturn);
+		logger.info("user should see the option for return");
+		slowDown();
+	}
+
+	@When("User click on Cancel Order Button")
+	public void UserClickOnCancelOrderButton() {
+		click(factory.homePage().cancelOrderSubmitBtn);
+		logger.info("User clicked on cancel button");
+	}
+
+	@Then("a cancelation message should be displayed ‘Your Order Has Been Cancelled’")
+	public void aCancelationMessageShouldBeDisplayedYourOrderHasBeenCancelled() {
+		Assert.assertTrue(isElementDisplayed(factory.homePage().yourOrderCancelled));
+		logger.info("Your Order Cancelled Successfully Message is displayed");
+	}
+
+	@When("User click on Return Items button")
+	public void userClickOnReturnItemsButton() {
+		click(factory.homePage().returnItemsBtn);
+		logger.info("User click on Return button item");
+		slowDown();
+	}
+
+	@When("User select the Return Reason ‘Item damaged’")
+	public void userSelectTheReturnReasonItemDamaged() {
+		click(factory.homePage().damgeItemReturn);
+		logger.info("User select the return reason");
+	}
+
+	@When("User select the drop off service ‘FedEx’")
+	public void userSelectTheDropOffServiceFedEx() {
+		click(factory.homePage().fedExDropoff);
+		logger.info("User select drop off service FedEx");
+	}
+
+	@When("User click on Return Order button")
+	public void userClickOnReturnOrderButton() {
+		click(factory.homePage().returnOrderBtn);
+		logger.info("User click on retuen order button");
+	}
+
+	@Then("a cancelation message should be displayed ‘Return was successful’")
+	public void aCancelationMessageShouldBeDisplayedReturnWasSuccessful() {
+		Assert.assertTrue(isElementDisplayed(factory.homePage().returnWasSuccessfully));
+		logger.info("Cancelation message should be displayed ‘Return was successful’");
+	}
+
+	@When("User click on Review button")
+	public void userClickOnReviewButton() {
+		click(factory.homePage().reviwBtn);
+		logger.info("User click on review button");
+	}
+
+	@When("User write Review headline {string} and {string}")
+	public void userWriteReviewHeadlineHeadlineValueAndReviewText(String headline, String review) {
+		sendText(factory.homePage().addaHeadline, headline);
+		sendText(factory.homePage().reiewDescription, review);
+		logger.info("User submit a review message");
+		slowDown();
+	}
+
+	@When("User click Add your Review button")
+	public void userClickAddYourReviewButton() {
+		click(factory.homePage().reviewSubmitBtn);
+		logger.info("User click on Add your Review button");
+		slowDown();
+	}
+
+	@Then("a review message should be displayed ‘Your review was added successfully’")
+	public void aReviewMessageShouldBeDisplayedYourReviewWasAddedSuccessfully() {
+		Assert.assertTrue(isElementDisplayed(factory.homePage().yourReviewMesg));
+		logger.info("Review message should be displayed ‘Return was successful’");
+	}
 }
-
-
--- src/test/resources/features/SignIn.feature
+++ src/test/resources/features/SignIn.feature
@@ -1,49 +1,91 @@
+@SignInTest
 Feature: Sign In Feature
 
-  #this is comment
   Background: 
     Given User is on retail website
     When User click on Sign in option
+    And User enter email 'Sohial456@gmail.com' and password 'Asdf123456!'
+    And User click on login button
+    And User should be logged in into Accout
 
-	 @signInTest
   Scenario: Verify user can sign in into Retail Application
-    And User enter email 'test@test.com' and password 'Tek@12345'
-    And User click on login button
-    Then User should be logged in into Account
+    And User enter email 'sohail.zahid12@gmail.com' and password 'Sohail123@'
+    And User click on login button    Then User should be logged in into Account
 
- 
-  Scenario Outline: Verify user can login with different email and pass
-    And User enter email <email> and password <password>
-    And User click on login button
-    Then User should be logged in into Account
-
-    Examples: 
-      | email                    | password    |
-      | 'ihateteacher@tek.com'   | 'Tek@12345' |
-      | 'panthers1@tekschool.us' | 'Tek@12345' |
-      | 'panthers2@tekschool.us' | 'Tek@12345' |
-
-  @signUpTest
+  @AccountCreate
   Scenario: Verify user can create an account into Retail Website
     And User click on Create New Account button
     And User fill the signUp information with below data
-      | name     | email                   | password  | confirmPassword |
-      | panthers | panthersxy@tekschool.us | Tek@12345 | Tek@12345       |
+      | name   | email               | password  | confirmPassword |
+      | Sohail | Sohial456@gmail.com | Asdf1234! | Asdf1234!       |
     And User click on SignUp button
     Then User should be logged into account page
 
-  @signUpTest
-  Scenario Outline: Verify user can create an account into Retail Website
-    And User click on Create New Account button
-    And User fill the signUp information with below data
-      | name   | email   | password   | confirmPassword   |
-      | <name> | <email> | <password> | <confirmPassword> |
-    And User click on SignUp button
-    Then User should be logged into account page
+  @UpdateAccount
+  Scenario: Verify User can update Profile Information
+    When User click on Account option
+    And User update Name 'Sohail Zahid' and Phone '5301234567'
+    And User click on Update button
+    Then user profile information should be updated
 
-    Examples: 
-      | name     | email                       | password  | confirmPassword |
-      | panthers | panthers112@tekschool.us    | Tek@12345 | Tek@12345       |
-      | panthers | panthersxy1123@tekschool.us | Tek@85236 | Tek@85236       |
-      | panthers | panthersxy1144@tekschool.us | Ket@8899  | Ket@8899        |
-      | panthers | panthersxy114@tekschool.us  | Tek@12345 | Tek@12345       |
+  @ChangePass
+  Scenario: Verify User can Update password
+    When User click on Account option
+    And User enter below information
+      | previousPassword | newPassword | confirmPassword |
+      | Asdf12345!       | Asdf123456! | Asdf123456!     |
+    And User click on Change Password button
+    Then a message should be displayed ‘Password Updated Successfully’
+
+  @Payment
+  Scenario: Verify User can add a payment method
+    When User click on Account option
+    And User click on Add a payment method link
+    And User fill Debit or credit card information
+      | cardNumber       | nameOnCard | expirationMonth | expirationYear | securityCode |
+      | 3214567898925445 | ShaSho     |              12 |           2024 |          456 |
+    And User click on Add your card button
+    Then a message should be displayed ‘Payment Method added successfully’
+
+  @UptadeCardInfo
+  Scenario: Verify User can edit Debit or Credit card
+    When User click on Account option
+    And User select card with ending '5445'
+    And User click on Edit option of card section
+    And user edit information with below data
+      | cardNumber       | nameOnCard | expirationMonth | expirationYear | securityCode |
+      | 1472583693214569 | Panthers   |              11 |           2026 |          123 |
+    And user click on Update Your Card button
+    Then a message should be displayed 'Payment Method updated Successfully'
+
+  @AddressInfo
+  Scenario: Verify User can add an Address
+    When User click on Account option
+    And User click on Add address option
+    And user fill new address form with below information
+      | country       | fullName | phoneNumber | streetAddress | apt | city     | state      | zipCode |
+      | United States | Sohail   |  4151234123 | 12 ABC Street |   1 | San Jose | California |   95050 |
+    And User click Add Your Address button
+    Then a message should be displayed ‘Address Added Successfully’
+
+  @RemoveCard
+  Scenario: Verify User can remove Debit or Credit card
+    When User click on Account option
+    And User click on the card added to his account
+    And User click on remove option of card section
+    Then payment details should be removed
+
+  @EditAaddress
+  Scenario: Verify User can edit an Address added on account
+    When User click on Account option
+    And user fill new address form with below information
+      | country       | fullName | phoneNumber | streetAddress | apt | city        | state      | zipCode |
+      | United States | Lashkari |  5301234568 | 123 ABCD Ave  |  35 | Santa Clara | California |   95050 |
+    And User click update Your Address button
+    Then a message should be displayed ‘Address Updated Successfully’
+
+  @removeAdress
+  Scenario: Verify User can remove Address from Account
+    When User click on Account option
+    And User click on remove option of Address section
+    Then Address details should be removed
